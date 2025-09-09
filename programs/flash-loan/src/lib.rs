269c3e12e53bb12d43ac9a93e1648963ce11dec3
Mardiprk <mk1659770@gmail.com>
2025-09-09 14:37:18 +0000

    flash

--- programs/flash-loan/src/lib.rs
+++ programs/flash-loan/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer};
 
 declare_id!("Gf5shq1VZ67fXkfw28r4TE422Uc9JTuJqXng1mxVHwuf");
 
@@ -6,11 +7,58 @@ declare_id!("Gf5shq1VZ67fXkfw28r4TE422Uc9JTuJqXng1mxVHwuf");
 pub mod flash_loan {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_pool(ctx: Context<InitializePool>, fee_rate: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn deposit_liquidity(ctx: Context<DepositLiquidity>, amount: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn flash_loan(ctx: Context<FlashLoan>, amount: u64, callback_data: Vec<u8>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn withdraw_liquidity(ctx: Context<WithdrawLiquidity>, amount: u64) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializePool<'info> {}
+
+#[derive(Accounts)]
+pub struct DepositLiquidity<'info> {}
+
+#[derive(Accounts)]
+pub struct FlashLoan<'info> {}
+
+#[derive(Accounts)]
+pub struct WithdrawLiquidity<'info> {}
+
+#[account]
+pub struct Pool {
+    pub authority: Pubkey,
+    pub token_mint: Pubkey,
+    pub token_vault: Pubkey,
+    pub fee_rate: u64,
+    pub total_liquidity: u64,
+    pub bump: u8,
+}
+
+impl Pool {
+    pub const LEN: usize = 32 + 32 + 32 + 8 + 8 + 1;
+}
+
+#[error_code]
+pub enum FlashLoanError {
+    #[msg("Insufficient liquidity in the pool")]
+    InsufficientLiquidity,
+
+    #[msg("Flash loan was not repaid")]
+    LoanNotRepaid,
+
+    #[msg("Unauthorized access")]
+    Unauthorized,
+}
+
